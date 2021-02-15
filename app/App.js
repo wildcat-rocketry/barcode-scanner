5be9979cb503fb48df57879fa4eafb59f0c3fadc
Zach Deibert <zachdeibert@gmail.com>
2021-02-14 22:32:10 -0600

    Added barcode scanning

--- app/App.js
+++ app/App.js
@@ -1,12 +1,45 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { notificationAsync, NotificationFeedbackType } from "expo-haptics";
+import sha1 from "sha1";
 
 export default function App() {
+    const [ hasPermission, setHasPermission ] = useState(null);
+    const [ lastScan, setLastScan ] = useState(null);
+    const [ lastHash, setLastHash ] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ _, data }) => {
+        if (lastScan !== data) {
+            notificationAsync(NotificationFeedbackType.Success);
+            const hash = sha1(data);
+            setLastScan(data);
+            setLastHash(hash);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
+            {(() => {
+                if (hasPermission === null) {
+                    return <Text style={styles.text}>Requesting for camera permission...</Text>;
+                }
+                if (hasPermission === false) {
+                    return <Text style={styles.text}>Camera permission access is required for scanning.</Text>;
+                }
+
+                return [
+                    <BarCodeScanner key="scanner" onBarCodeScanned={handleBarCodeScanned} style={StyleSheet.absoluteFillObject} />,
+                    <Text key="scanned" style={styles.textBottom}>{lastScan !== null && `Last Scan: ${lastScan}\nLast Hash: ${lastHash}`}</Text>
+                ];
+            })()}
         </View>
     );
 }
@@ -14,8 +47,16 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#FFFFFF",
+        flexDirection: "row",
+        backgroundColor: "#000000",
         alignItems: "center",
         justifyContent: "center",
+    },
+    text: {
+        color: "#FFFFFF",
+    },
+    textBottom: {
+        color: "#FFFFFF",
+        alignSelf: "flex-end",
     },
 });
